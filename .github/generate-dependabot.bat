ee217f1eaf925cdb109a1fc28710e081e5c1f2de
Daniel Christensen <xdach@energinet.dk>
2021-04-09 08:16:44 +0200

    More progress

--- .github/generate-dependabot.bat
+++ .github/generate-dependabot.bat
@@ -1,23 +1,39 @@
 @echo off
-set "working_dir=%~dp0"
+setlocal
 
+set testvar="C:\Projects\Energinet\geh-market-roles\source\shared\messaging\source\GreenEnergyHub.Messaging.Protobuf.Tests\GreenEnergyHub.Messaging.Protobuf.Tests.csproj:Projects=tests"
+set output=%testvar:Projects=tests%
+echo Res: %output%
+goto :eof
+
+
+call :setDirVariables
+echo %script_dir%
+echo %parent_dir%
 call :generateFile
 call :run
 echo Done
+endlocal
 goto :eof
 
 :generateFile
-type NUL >%working_dir%/testfile.yml
+type NUL >%script_dir%/testfile.yml
 goto :eof
 
 :run
-pushd %~dp0..
-echo Current dir: %~dp0
+cd %parent_dir%
 for /R %%f in (*.csproj) do call :insertSection "%%f"
+cd %script_dir%
 goto :eof
 
 :insertSection
 echo %~1
-set proj_path=%~1
-echo %proj_path%>>%working_dir%/testfile.yml
+call set proj_path=%%~1:Projects=tests%
+echo %proj_path%>>%script_dir%/testfile.yml
+goto :eof
+
+:setDirVariables
+set "script_dir=%~dp0"
+pushd %script_dir%..
+set "parent_dir=%cd%"
 goto :eof
